85c10c9cb9f21cb7722dec38b7085ae1991bafe5
Maximiliano <Fnievas68@gmail.com>
2025-02-28 18:27:27 -0300

    Act de datos para despliegue

--- back/src/routers/adminRouter.ts
+++ back/src/routers/adminRouter.ts
@@ -3,7 +3,9 @@ import { AdminController } from '../controllers/adminController';
 import { asyncHandler } from '../middleware/asyncHandler';
 
 const adminRouter = Router();
-
+adminRouter.get("/", (req, res) => {
+    res.send("Admin API funcionando correctamente");
+  });
 adminRouter.post('/register', asyncHandler(AdminController.createAdmin));
 adminRouter.post('/login', asyncHandler(AdminController.login));
 
