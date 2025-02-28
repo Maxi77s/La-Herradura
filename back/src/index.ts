a5e4ae8213a49a6e2a38d3f88135e2ef7a32d0d3
Maximiliano <Fnievas68@gmail.com>
2025-02-28 18:18:24 -0300

    act de cors y .env

--- back/src/index.ts
+++ back/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import appointmentRouter from './routers/appointmentRouter';
 
-dotenv.config();
+dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.envproduction' : '.env' });
 
 const app = express();
 
@@ -13,6 +13,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://la-herradura-flax.vercel.app",
   "https://la-herradura-gg1q9spev-e-commerces-projects-7a8d629e.vercel.app",
+  "https://la-herradura-production.up.railway.app",
   /\.vercel\.app$/ // Permite cualquier subdominio en Vercel
 ];
 
