4e0dd70f65e402bc5ecf70f63fbea4e3ea8524dc
Maximiliano <Fnievas68@gmail.com>
2025-03-11 12:12:47 -0300

    Act de cors para postgres

--- back/src/server.ts
+++ back/src/server.ts
@@ -13,6 +13,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://la-herradura-flax.vercel.app",
   "https://la-herradura-production.up.railway.app",
+  "https://postgres-production-cad4.up.railway.app/",
   /\.vercel\.app$/ // Permite cualquier subdominio en Vercel
 ];
 
